8d3a0e5a78dfdc8e517764bc974bb79b32b9ff2b
Laura Juiz <juiz.laurai@gmail.com>
2019-10-24 02:39:50 -0300

    Agregue los staticmethods al fin

--- Viajes/Transporte/transporte.py
+++ Viajes/Transporte/transporte.py
@@ -18,8 +18,12 @@ class Transporte:
     def dar_ingreso(self):
         pass
 
+    @staticmethod
     def listar_pasajeros(self):
-        pass
+        print('\nPatente: ' + self.__patente)
+        for valor in range(len(self.__pasajeros)):
+            print(self.__pasajeros[valor])
+        print('Ocupados: '+str(len(self.__pasajeros))+'\nLibres: '+str(self.__capacidad - len(self.__pasajeros)))
 
     def dar_consumo(self):
         pass
