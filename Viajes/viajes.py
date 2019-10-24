a4b0c298c7bcfcb31d87214eb826efb3bc375c11
Laura Juiz <juiz.laurai@gmail.com>
2019-10-24 05:07:03 -0300

    Con staticmethods y algunas otras func

--- Viajes/viajes.py
+++ Viajes/viajes.py
@@ -7,7 +7,7 @@ s2 = segmento.Segmento("Liniers", "Lujan", 180, 300, 70)
 s3 = segmento.Segmento("Lujan", "Chacabuco", 275, 120, 175)
 s4 = segmento.Segmento("Chacabuco", "Junin", 175,0,60)
 
-r = recorrido.Recorrido("ida Retiro-Junin")
+r = recorrido.Recorrido("Ida Retiro - Junin")
 r.agregar_segmento(s1)
 r.agregar_segmento(s2)
 r.agregar_segmento(s3)
@@ -24,4 +24,11 @@ t.agregar_pasajero(pax1)
 t.agregar_pasajero(pax2)
 t.agregar_pasajero(pax3)
 
+print('\nEl total del recorrido tiene una longitud de '+ str(recorrido.Recorrido.dar_longitud(r))+' Km.')
+#print Esta_completo !!! Preguntar a que se refiere con esto, los huecos del recorrido?
 transporte.Transporte.listar_pasajeros(t)
+
+print('\nEl tiempo mínimo en completar el recorrido es de: '+str(recorrido.Recorrido.dar_tiempo_minimo(r, t))+' Horas')
+print('El total del recorrido consume '+str(recorrido.Recorrido.dar_consumo(r,t))+' Litros de Combustible.')
+
+# pasajero.Pasajero.dar_precio(t) # ESTO ES PARA SACAR EL PRECIO DE CADA PASAJERO, TODAVIA NO ENTIENDO LA LOGICA
