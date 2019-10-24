0218d9a664218f8ff0f1faaf90d8c4e66ef122ac
Laura Juiz <juiz.laurai@gmail.com>
2019-10-24 02:35:06 -0300

    Agregue los staticmethods al fin

--- Viajes/viajes.py
+++ Viajes/viajes.py
@@ -13,4 +13,15 @@ r.agregar_segmento(s2)
 r.agregar_segmento(s3)
 r.agregar_segmento(s4)
 
-print(recorrido.Recorrido.dar_recorrido)
+recorrido.Recorrido.dar_recorrido(r)
+
+pax1 = pasajero.Pasajero(23123123,'Ana','Liniers','Chacabuco')
+pax2 = pasajero.Pasajero(54321321, 'Pablo', 'Retiro', 'Junin')
+pax3 = pasajero.Pasajero(43432433, 'Carla', 'Retiro', 'Chacabuco')
+
+t = transporte.Transporte('OVD543', 62, 0.85, 90)
+t.agregar_pasajero(pax1)
+t.agregar_pasajero(pax2)
+t.agregar_pasajero(pax3)
+
+transporte.Transporte.listar_pasajeros(t)
